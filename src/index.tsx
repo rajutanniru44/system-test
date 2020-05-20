e9baf99907b6c27f35df6e9bbf8b138f6d672db2
Tanniru Ankamma Raju <rajutanniru44@gmail.com>
2020-05-20 14:30:24 +0530

    Added redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+// import App from './components/App';
+import App from './maincomponents/App';
 import ErrorBoundary from './components/ErrorBoundary';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import combineReducers from './reducers/index';
+
+const store = createStore(combineReducers)
 
 
-ReactDOM.render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById('root'));
+ReactDOM.render(<ErrorBoundary><Provider store={store}><App /></Provider></ErrorBoundary>, document.getElementById('root'));
---
+++ src/maincomponents/App.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Footer from './Footer'
+import AddTodo from '../container/AddTodo'
+import VisibleTodoList from '../container/VisibleTodoList'
+import styles from '../index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import AddModal from '../common/components/AddModal';
+import { addTodo } from 'actions';
+
+
+interface State {
+    hideModal: boolean,
+    // data: Array<Task>,
+    // activeSection: string,
+    isReadOnly: boolean,
+    taskObj: Task
+}
+
+interface Task {
+    currentState: boolean,
+    title: string,
+    description: string
+    createdAt: Date
+    dueDate: Date
+    priority: string
+}
+
+class App extends React.Component<{}, State> {
+    state: State;
+    taskList: Array<Task> = [];
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            hideModal: false,
+            isReadOnly: false,
+            taskObj: {
+                currentState: false,
+                title: "",
+                description: "",
+                createdAt: new Date(),
+                dueDate: new Date(),
+                priority: ""
+            }
+        }
+    }
+
+    _openAddModal = () => {
+        this.setState({ hideModal: true })
+    }
+
+    onConfirm = (data: boolean) => {
+        this.setState({
+            hideModal: false
+        })
+    }
+
+    _onClose = () => {
+        this.setState({ hideModal: false })
+    }
+
+    render() {
+        const { hideModal, isReadOnly, taskObj } = this.state;
+
+        return (
+            <>
+                <Footer />
+                <VisibleTodoList />
+                <AddTodo />
+
+                {hideModal ? <AddModal onConfirm={this.onConfirm} onClose={this._onClose} isReadOnly={isReadOnly} taskObj={taskObj} isDataAvailable={false} /> : ""}
+                <div className={styles.bottomRightAlign}>
+                    <FontAwesomeIcon icon={faPlus} onClick={this._openAddModal} />
+                </div>
+            </>
+        )
+    }
+}
+
+
+export default App;
